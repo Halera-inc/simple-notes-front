bd13166e58eca6270c7c40bc8b875d33bffb0838
IvanFestina <ivanfestina@gmail.com>
2022-08-18 00:09:41 +0300

    feature: store * add store, notesSlice, profileSlice, types, hooks(useAppDispatch, useAppSelector) [HALERA-5]

---
+++ src/bll/slices/notesSlice.ts
@@ -0,0 +1,18 @@
+import {createSlice} from '@reduxjs/toolkit'
+import {NoteTextType, NoteTodoType} from 'src/utils/types'
+
+
+const initialState = {
+    notes: [] as Array<NoteTextType | NoteTodoType>
+}
+
+export const notesSlice = createSlice({
+    name: 'notes',
+    initialState,
+    reducers: {},
+    extraReducers: {}
+})
+
+export const {} = notesSlice.actions
+
+export default notesSlice.reducer
