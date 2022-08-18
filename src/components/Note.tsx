148ad779c2b2cca6489d300e04d3ae78dde2d773
sveta <Sunkovasvetlana@gmail.com>
2022-08-18 13:39:10 +0300

    fix:note.tsx color

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,9 +1,9 @@
 import s from './../styles/Note.module.css'
 
-const Note = (props:{titleNode:string,textNode:string}) => {
+const Note = (props:{titleNode:string,textNode:string,style:any}) => {
     return (
-        <div className={s.card}>
-                <h2 className={s.cardTitle}>{props.titleNode}</h2>
+        <div className={s.card} style={props.style}>
+                <h2 className={s.cardTitle} >{props.titleNode}</h2>
                 <p>{props.textNode}</p>
                 <div className={s.cardAction}>
                     <img className={s.editIcon}/>
