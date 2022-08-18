e1ef92996c5af0f841091793f85cbfe7e5aeef74
sveta <Sunkovasvetlana@gmail.com>
2022-08-18 12:52:18 +0300

    feat:create note.tsx

--- pages/notes.tsx
+++ pages/notes.tsx
@@ -1,10 +1,13 @@
 import MainContainer from "../src/components/MainContainer";
+import Note from "../src/components/Note";
 
 const Notes = () => {
     return (
         <MainContainer>
             <div>
                 <h1>My notes</h1>
+                <Note textNode={'Hello, you are my frends  '} titleNode={'your note'}/>
+
             </div>
         </MainContainer>
     )
