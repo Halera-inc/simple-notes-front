18ecabf1c0b8cb2c9bfcd2bec5b4a65a65f68279
andreiryaskov <andreiryaskov@gmail.com>
2022-08-20 16:40:27 +0300

    fix: переверстал сайдбар

--- src/assets/images/LoginIcon.tsx
+++ src/assets/images/LoginIcon.tsx
@@ -1,11 +1,10 @@
 import React, {SVGProps} from 'react'
-
+import s from "../../components/Sidebar/Sidebar.module.css";
 export function LoginIcon(props: SVGProps<SVGSVGElement>) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="40px" height="40px"
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" className={s.interface_icon__style}
              viewBox="0 0 20 20" {...props}>
-            <path fill="#E5E5E5" fillRule="evenodd"
-                  d="M3 3a1 1 0 0 1 1 1v12a1 1 0 1 1-2 0V4a1 1 0 0 1 1-1Zm7.707 3.293a1 1 0 0 1 0 1.414L9.414 9H17a1 1 0 1 1 0 2H9.414l1.293 1.293a1 1 0 0 1-1.414 1.414l-3-3a1 1 0 0 1 0-1.414l3-3a1 1 0 0 1 1.414 0Z"
+            <path d="M3 3a1 1 0 0 1 1 1v12a1 1 0 1 1-2 0V4a1 1 0 0 1 1-1Zm7.707 3.293a1 1 0 0 1 0 1.414L9.414 9H17a1 1 0 1 1 0 2H9.414l1.293 1.293a1 1 0 0 1-1.414 1.414l-3-3a1 1 0 0 1 0-1.414l3-3a1 1 0 0 1 1.414 0Z"
                   clipRule="evenodd"></path>
         </svg>
     )
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import s from './Sidebar.module.css';
-import SettingIcon from "../../assets/images/SettingIcon";
 import LoginIcon from "../../assets/images/LoginIcon";
-import MenuIcon from "../../assets/images/MenuIcon";
-import UserIcon from "../../assets/images/UserIcon";
-import PencilAltIcon from "../../assets/images/PencilAltIcon";
+import {PlusIcon} from "../../assets/images/PlusIcon";
+import {SunnyIcon} from "../../assets/images/SunnyIcon";
+import {MenuIcon} from "../../assets/images/MenuIcon";
+import {SettingIcon} from "../../assets/images/SettingIcon";
+import {PencilIcon} from "../../assets/images/PencilIcon";
 
 export const Sidebar = (): JSX.Element => {
     return (
         <div className={s.sidebar_wrapper}>
-            <div className={s.icons_wrapper}>
-                <div className={s.addNote_icon}>
-                    <PencilAltIcon/>
-                </div>
-                <div className={s.interface_icons}>
-                    <MenuIcon/>
-                    <SettingIcon/>
-                    <UserIcon/>
-                    <LoginIcon/>
-                </div>
-                <div className={s.copyright_text}>
-                    © 2022<br/>
-                    Halera dev.
-                </div>
+            <div className={s.addNote_icon}>
+                <PencilIcon width={"5em"} height={"5em"}/>
             </div>
+            <div className={s.interface_icons}>
+                <PlusIcon/>
+                <MenuIcon/>
+                <SettingIcon/>
+            </div>
+            <div className={s.login_block}>
+                <SunnyIcon/>
+                <LoginIcon/>
+            </div>
+            <div className={'bg'}></div>
         </div>
     );
 };
