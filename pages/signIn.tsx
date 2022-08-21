b62901b0c74ac647ae5d286b862eafee34885dff
sveta <Sunkovasvetlana@gmail.com>
2022-08-21 19:51:44 +0300

    feat:refactor signIn

--- pages/signIn.tsx
+++ pages/signIn.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MainContainer from "../src/components/MainContainer";
 import {useFormik} from "formik";
 import s from './../src/styles/SignIn.module.css'
@@ -8,6 +8,8 @@ import ArrowBackIcon from "../src/assets/images/ArrowBackIcon";
 import Link from "next/link";
 
 const SignIn = () => {
+
+    const [isLogin,setIsLogin]=useState(true);
 
     type FormikErrorType = {
         username?: string
@@ -22,20 +24,21 @@ const SignIn = () => {
 
 
         },
-        validate: (values) => {
-            const errors: FormikErrorType = {};
-            if (!values.username) {
-                errors.username = 'Required';
-            }
-            if (!values.password) {
-                errors.password = 'Required';
-            } else if (values.password.length <= 7) {
-                errors.password = 'Password must be more than 7 characters...'
-            }
-            return errors;
-        },
+        // validate: (values) => {
+        //     const errors: FormikErrorType = {};
+        //     if (!values.username) {
+        //         errors.username='  ';
+        //
+        //     }
+        //     if (!values.password) {
+        //         errors.password = '   ';
+        //     }
+        //     return errors;
+        // },
         onSubmit: values => {
             // dispatch(setRegistrTC(values));
+            alert(JSON.stringify(values));
+            setIsLogin(false);
             formik.resetForm();
         },
     })
@@ -45,7 +48,7 @@ const SignIn = () => {
                 <div className={s.cardC}>
                     <div className={s.cardBody}>
                         <div className={s.wrapperTitle}>
-                            <h2 className={s.cardTitle}> Sing In</h2>
+                            <h2 className={s.cardTitle}> Sign In</h2>
                             <div className={s.arrowIcon}>
                                 <Link href={"/"}>
                                 <ArrowBackIcon width={'2.5em'} height={'2.5em'} color={'#5590C1'}/>
@@ -55,28 +58,28 @@ const SignIn = () => {
                         <form onSubmit={formik.handleSubmit}>
                             <div className={`${s.formControl} ${s.one}`}>
                                 <label className={s.label}>
-                                    <UserIcon width={'3em'} height={'3em'} color={!formik.errors.username ?'#5590C1':'#F06464'}/>
+                                    <UserIcon width={'3em'} height={'3em'} color={isLogin ?'#5590C1':'#F06464'}/>
                                     <input type="text" id='username' placeholder="username"
-                                           className={!formik.errors.username ? s.inputI: s.errorInput}
+                                           className={isLogin ? s.inputI: s.errorInput}
                                            {...formik.getFieldProps('username')}/>
                                 </label>
-                                {formik.touched.username && formik.errors.username ?
-                                    <div style={{color: "red",marginTop:'10px'}}>{formik.errors.username}</div> : null}
+                                {formik.touched.username && formik.errors.username}
+
                             </div>
 
                             <div className={`${s.formControl} ${s.two}`}>
                                 <label className={s.label}>
-                                    <KeyIcon width={'3em'} height={'3em'} color={!formik.errors.password ?'#5590C1':'#F06464'}/>
+                                    <KeyIcon width={'3em'} height={'3em'} color={isLogin ?'#5590C1':'#F06464'}/>
                                     <input type="text" id='password' placeholder="password"
-                                           className={!formik.errors.password ? s.inputI: s.errorInput}
+                                           className={isLogin ? s.inputI: s.errorInput}
                                            {...formik.getFieldProps('password')}/>
                                 </label>
-                                {formik.touched.password && formik.errors.password ?
-                                    <div style={{color: "red",marginTop:'10px'}}>{formik.errors.password}</div>: null}
+                                {!isLogin ?
+                                    <div className={s.errorText}>Incorrect login or password!</div>: null}
                             </div>
 
                             <div className="card-actions justify-center">
-                                <button className={s.btnB}>Login</button>
+                                <button type={'submit'} className={s.btnB}>Login</button>
                             </div>
                         </form>
                         <p className={s.text}>Registration</p>
