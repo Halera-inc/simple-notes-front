1397256d8c91097d5f72554e5cb83c44d1f84fda
IvanFestina <ivanfestina@gmail.com>
2022-08-23 20:33:35 +0300

    bug: merge successful

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -12,14 +12,6 @@ type NotePropsType = {
 
 const Note = ({color, title, text, edit}: NotePropsType) => {
 
-    if (color === 'blue') color = "#5590C1"
-    if (color === 'green') color = "#5E9C80"
-    if (color === 'violet') color = "#866FA7"
-    if (color === 'mustard') color = "#C6B05D"
-    if (color === 'dark') color = "#444343"
-    if (color === 'default') color = "#4e5458"
-
-
     return (
         <>
             <div className={s.card} onClick={()=>edit(title,text)}>
