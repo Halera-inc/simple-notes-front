0944c4c7b1c9f0d49c57be57ad4f671de4be524e
Oleg <shevchenko.oleg.87@gmail.com>
2022-08-24 21:45:55 +0300

    Merge branch 'feat/add_universal_header' of /Users/oleg/programming_training/IT-Incubator/halera/simple-notes-front with conflicts.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import SearchIcon from "../../assets/images/SearchIcon";
+import UserCircleIcon from "../../assets/images/UserCircleIcon";
+import {useState} from "react";
+import DropDown from "./DropDown";
+
+
+const Header = () => {
+
+    const liArray = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7']
+    const [isCollapsed, setIsCollapsed] = useState(false)
+    const collapsedHandler = () => {
+    }
+
+    return (
+        <div className='bg-orange-500 flex justify-between items-center h-14'>
+            <p className='px-6 text-xl'>Header</p>
+            <div className='flex justify-around w-96 items-center'>
+                <SearchIcon width={'1.5em'} height={'1.5em'}/>
+                <UserCircleIcon width={'3em'} height={'3em'}/>
+                <p>Ivanov S.</p>
+                <DropDown isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} dropItems={liArray} collapsed={collapsedHandler}/>
+            </div>
+        </div>
+    )
+        ;
+};
+
+export default Header;
--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,16 +1,27 @@
 import s from './../styles/Note.module.css'
 import EditIcon from "../assets/images/EditIcon";
 import DeleteIcon from "../assets/images/DeleteIcon";
+import ModalWindow from "./ModalWindow";
+import {useEffect, useRef, useState} from "react";
+import {useAppSelector} from "../utils/hooks";
 
 
+type ModalWindowType = 'edit' | 'create'
 type NotePropsType = {
     title: string | null
     text: string | null
     color: string
-    edit: (title: string | null, text:string | null) => void
+    id: number
 }
 
-const Note = ({color, title, text, edit}: NotePropsType) => {
+const Note = ({color, title, text, id}: NotePropsType) => {
+
+    const isModalShow = useAppSelector((state) => state.notes.createNoteModal)
+    const [modalType, setModalType] = useState<ModalWindowType>('edit')
+    const modalBtnRef = useRef<HTMLLabelElement>(null)
+    const refId = `show-modal-btn-${id}`
+    const modalWindowId = `my-modal-${id}`
+
 
     if (color === 'blue') color = "#5590C1"
     if (color === 'green') color = "#5E9C80"
@@ -20,10 +31,33 @@ const Note = ({color, title, text, edit}: NotePropsType) => {
     if (color === 'default') color = "#4e5458"
 
     //
+    useEffect(() => {
+        debugger
+        isModalShow && onAddNoteClickHandler()
+    }, [isModalShow])
+
+
+    const onAddNoteClickHandler = () => {
+        debugger
+        setModalType("create")
+        console.log(modalBtnRef.current)
+        modalBtnRef.current && modalBtnRef.current.click()
+    }
+    const onCardClickHandler = () => {
+        setModalType("edit")
+        modalBtnRef.current && modalBtnRef.current.click()
+    }
 
     return (
         <>
-            <div className={s.card} onClick={()=>edit(title,text)}>
+            <label ref={modalBtnRef} id={refId} htmlFor={modalWindowId} className="btn modal-button hidden">open
+                modal</label>
+            <ModalWindow titleNode={title ? title : ''}
+                         textNode={text ? text : ''}
+                         modalType={modalType}
+                         refId={id}
+                         modalWindowId={modalWindowId}/>
+            <div className={s.card} onClick={onCardClickHandler}>
                 <h2 className={s.cardTitle}>{title}</h2>
                 <p>{text}</p>
                 <div className={s.cardAction}>
@@ -31,6 +65,7 @@ const Note = ({color, title, text, edit}: NotePropsType) => {
                     <DeleteIcon/>
                 </div>
             </div>
+            {/*<button className='btn mx-8 my-8' onClick={onAddNoteClickHandler}>Add Note</button>*/}
         </>
     )
 }
