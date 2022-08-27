82e9860b8936173a29d2e9fe10be31779a3ca093
marinadegames <marinadegames@gmail.com>
2022-08-28 00:01:58 +0300

    fix deleted nav dev

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -17,16 +17,6 @@ const MainContainer = (props: MainContainerTypeProps) => {
     return (
         <>
             {headerRoots.includes(currentRoute) ? <Header/> : null}
-            <div className="navbar absolute left-50 bottom-0 border-2 border-red flex justify-center">
-                <h1><b>DEV MENU:</b></h1>
-                <NavItem text={'Landing'} href={"/"}/>
-                <NavItem text={'My notes'} href={"/notes"}/>
-                <NavItem text={'Settings'} href={"/settings"}/>
-                <NavItem text={'About'} href={"/about"}/>
-                <NavItem text={'404'} href={"/404"}/>
-                <NavItem text={'Registration'} href={"/registration"}/>
-                <NavItem text={'SingIn'} href={"/signIn"}/>
-            </div>
             <div>{props.children}</div>
         </>
     );
