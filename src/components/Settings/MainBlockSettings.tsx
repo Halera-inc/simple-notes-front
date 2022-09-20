be984a5e85dc298a5b42ea5272364b8f2dedefcc
Oleg <shevchenko.oleg.87@gmail.com>
2022-09-20 16:49:15 +0300

    fix: работа над версткой компонента settings [HALERA-94]

--- src/components/Settings/MainBlockSettings.tsx
+++ src/components/Settings/MainBlockSettings.tsx
@@ -6,6 +6,7 @@ import InputForm from "src/components/Settings/inputform/InputForm";
 import {useSession} from "next-auth/react";
 import {OtherSettings} from "./OtherSettings";
 import {UserType} from "../../utils/types";
+import Image from 'next/image';
 
 
 const MainBlockSettings = () => {
@@ -21,16 +22,19 @@ const MainBlockSettings = () => {
         <div className={s.wrapperMaimSettings}>
             <div className={s.leftArea}>
                 <div className={s.myProfile}>
-                    {/*<div className={s.imgProfileWrapper}>*/}
-                        <img style={{width: 200, height: 200, border: '2px solid var(--blue-dark)' }}
-                             alt={'avatar'}
-                             src={`${session?.user?.image}`}/>
+                    <div className={s.imgProfileWrapper}>
+                        <Image
+                            style={{width: 200, height: 200, border: '2px solid var(--blue-dark)'}}
+                            alt={'avatar'}
+                            src={`${session?.user?.image}`}
+                        />
+
                     {/*Пока убрал Image так как не знаю как правильно прописывать в next.config.js (Ваня)*/}
                         {/*<Image src={`${session?.user?.image}`}*/}
                         {/*       width={200}*/}
                         {/*       height={200}*/}
                         {/*       alt={'avatar'}/>*/}
-                    {/*</div>*/}
+                    </div>
                     <div className={s.editWrapper}>
                         <ul className={s.editData}>
                             <li className={s.myName}>{session?.user?.email}</li>
@@ -38,7 +42,7 @@ const MainBlockSettings = () => {
                         </ul>
                         <Button title={'Edit'}
                                 callback={editProfileHandler}
-                                style={{margin: 0}}/>
+                                style={{margin: 0, width: '104px'}}/>
                     </div>
                 </div>
                 {edit ? <InputForm/> : ''}
