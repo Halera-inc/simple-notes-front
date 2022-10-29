4f47be1c3de5f6aacfd5b2a9b12b651e0964ced4
Oleg <shevchenko.oleg.87@gmail.com>
2022-10-29 15:31:40 +0300

    initial commit with grid layout library

--- pages/notes.tsx
+++ pages/notes.tsx
@@ -9,9 +9,13 @@ import {colorizedColorType} from "../src/components/Note";
 import {ColorSamplesType} from "../src/api/notes-api";
 import {getSession} from "next-auth/react";
 import {GetServerSideProps, GetServerSidePropsContext} from "next";
+import {layout, SpringGrid, measureItems, CSSGrid, makeResponsive } from 'react-stonecutter';
 
 const Notes = () => {
-
+    const Grid = makeResponsive(measureItems(CSSGrid), {
+        maxWidth: 1920,
+        minPadding: 100
+    });
     const dispatch = useAppDispatch()
     const notes = useAppSelector(state => state.notes.notes)
     const searchParams = useAppSelector(state => state.notes.searchParams)
@@ -85,19 +89,33 @@ const Notes = () => {
 
             <div className={"dark:bg-grey p-l-[100px] h-[100%] min-h-[100vh]"}>
                 <div className={s.notesBlock}>
+                    <Grid
+                        component="ul"
+                        columns={2}
+                        columnWidth={250}
+                        gutterWidth={10}
+                        gutterHeight={10}
+                        layout={layout.pinterest}
+                        duration={500}
+                        easing="ease-out"
+                    >
+
                     {notes && notes.filter(n => n.title && n.title.toLowerCase().includes(searchParams.toLowerCase())
                         || n.note_text && n.note_text.toLowerCase().includes(searchParams.toLowerCase())).map((n, i) =>
-                        <Note key={n._id}
-                              title={n.title}
-                              note_text={n.note_text}
-                              color={n.color}
-                              noteId={n._id}
-                              edit={onCardClickHandler}
-                              createdAt={n.createdAt}
-                              index={i}
-                              moveCard={moveCards}
-                        />
+                        <li key={n._id}>
+                            <Note key={n._id}
+                                  title={n.title}
+                                  note_text={n.note_text}
+                                  color={n.color}
+                                  noteId={n._id}
+                                  edit={onCardClickHandler}
+                                  createdAt={n.createdAt}
+                                  index={i}
+                                  moveCard={moveCards}
+                            />
+                        </li>
                     )}
+                    </Grid>
                 </div>
             </div>
         </MainContainer>
