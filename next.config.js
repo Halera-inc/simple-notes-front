ddce16c4004f687dc0aa728e991de3aa0ecdb7bc
Oleg <shevchenko.oleg.87@gmail.com>
2022-11-27 14:43:37 +0300

    implemented PWA in simple notes app

--- next.config.js
+++ next.config.js
@@ -1,24 +1,40 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa');
+// const withPWA = require('next-pwa');
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === 'development',    
+//   }
+//   // images: {
+//   //   remotePatterns: [
+//   //     {
+//   //       protocol: 'https',
+//   //       hostname: '**.example.com',
+//   //     },
+//   //     ]
+//   // },
+// })
+
+
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+});
 
 const nextConfig = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',    
-  }
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: '**.example.com',
-  //     },
-  //     ]
-  // },
-})
+  // next config
+});
+module.exports = nextConfig;
 
-module.exports = nextConfig
+// module.exports = nextConfig
 
--- src/components/RouteGuard.tsx
+++ src/components/RouteGuard.tsx
@@ -1,49 +1,51 @@
-import {useRouter} from "next/router";
-import {useEffect, useRef, useState} from "react";
-import {useAppDispatch, useAppSelector} from "../utils/hooks";
-import {initializeApp} from "../bll/slices/authSlice";
-import Sidebar from "./Sidebar/Sidebar";
-import {Spinner} from "./Spinner";
+// import {useRouter} from "next/router";
+// import {useEffect, useRef, useState} from "react";
+// import {useAppDispatch, useAppSelector} from "../utils/hooks";
+// import {initializeApp} from "../bll/slices/authSlice";
+// import Sidebar from "./Sidebar/Sidebar";
+// import {Spinner} from "./Spinner";
+//
+// export const RouteGuard = ({children}: any) => {
+//
+//
+//     const {isLoggedIn, isInitialized} = useAppSelector(state => state.auth)
+//     const dispatch = useAppDispatch()
+//
+//     useEffect(() => {
+//         if (!effectRan.current) {
+//             dispatch(initializeApp())
+//         }
+//         return () => {
+//             effectRan.current = true
+//         }
+//     }, [dispatch])
+//
+//
+//     return (
+//         <>
+//             {!isInitialized
+//                 ?
+//                 <div style={{
+//                     width: "100%",
+//                     height: '100vh',
+//                     display: 'flex',
+//                     justifyContent: 'center',
+//                     alignItems: "center"
+//                 }}>
+//                     <Spinner size={'300px'}/>
+//                 </div>
+//                 :
+//                 <div>
+//                     {isLoggedIn && <Sidebar/>}
+//                     <div className='flex flex-col bg-white dark:bg-black'>
+//                         {children}
+//                     </div>
+//                 </div>
+//             }
+//         </>
+//     )
+//
+//
+// }
 
-export const RouteGuard = ({children}: any) => {
-
-
-    const {isLoggedIn, isInitialized} = useAppSelector(state => state.auth)
-    const dispatch = useAppDispatch()
-
-    useEffect(() => {
-        if (!effectRan.current) {
-            dispatch(initializeApp())
-        }
-        return () => {
-            effectRan.current = true
-        }
-    }, [dispatch])
-
-
-    return (
-        <>
-            {!isInitialized
-                ?
-                <div style={{
-                    width: "100%",
-                    height: '100vh',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: "center"
-                }}>
-                    <Spinner size={'300px'}/>
-                </div>
-                :
-                <div>
-                    {isLoggedIn && <Sidebar/>}
-                    <div className='flex flex-col bg-white dark:bg-black'>
-                        {children}
-                    </div>
-                </div>
-            }
-        </>
-    )
-
-
-}
+export default ()=>{}
