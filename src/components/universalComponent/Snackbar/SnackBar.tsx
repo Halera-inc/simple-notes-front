a400dfe20d309a6a0651d04bdc0e3e425110891b
DimaMaslo88 <dmitrij-maslo@mail.ru>
2023-02-02 16:02:52 +0300

    'feat/Halera-128-Snackbar for ava-fixed5'

--- src/components/universalComponent/Snackbar/SnackBar.tsx
+++ src/components/universalComponent/Snackbar/SnackBar.tsx
@@ -1,30 +1,38 @@
-import React,{useEffect} from 'react';
-import {useAppSelector,useAppDispatch} from "src/utils/hooks";
+import React, {useEffect} from 'react';
+import {useAppSelector, useAppDispatch} from "src/utils/hooks";
 import {setIsAppSuccess} from '../../../bll/slices/appSlice';
-type SnackBarType={
-    title:string
+
+type SnackBarType = {
+    title: string
 }
-export const SnackBar = ({title}:SnackBarType) => {
+export const SnackBar = ({title}: SnackBarType) => {
     const dispatch = useAppDispatch()
-     const avatarIsSavedStatus = useAppSelector(state=>state.app.isAppSuccess.status)
-     // const avatarIsSavedTitle = useAppSelector(state=>state.app.isAppSuccess.title)
+    const avatarIsSavedStatus = useAppSelector(state => state.app.isAppSuccess.status)
+    // const avatarIsSavedTitle = useAppSelector(state=>state.app.isAppSuccess.title)
     console.log(avatarIsSavedStatus)
     useEffect(() => {
         if (avatarIsSavedStatus) {
             setTimeout(() => {
-                dispatch(setIsAppSuccess({status: false,title:''}))
+                dispatch(setIsAppSuccess({status: false, title: ''}))
             }, 1500)
         }
     }, [avatarIsSavedStatus])
-   if ( avatarIsSavedStatus) {
-    return (
+    if (avatarIsSavedStatus) {
+        return (
 
             <div className="alert alert-info shadow-lg">
-        <div >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <span>{title}</span>
-        </div>
-    </div> )} else <></>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         className="stroke-current flex-shrink-0 w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                    </svg>
+                    <span>{title}</span>
+                </div>
+            </div>)
+    } else {
+        return <></>
+    }
 
 
 };
